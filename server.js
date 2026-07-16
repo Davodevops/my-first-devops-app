6147d817779121009b0caaacd91c353082068fca
Mraz DevOps <misha111191@inbox.ru>
2026-07-16 22:14:33 +0300

    Add GitHub Actions workflow

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-Hello DevOps!
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  res.end('Привет! Этот сайт запущен внутри моего первого Docker-контейнера! 🎉\n');
+});
+
+server.listen(port, () => {
+  console.log(`Сервер успешно запущен на порту ${port}`);
+});
